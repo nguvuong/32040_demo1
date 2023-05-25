07fb134ba7c850567b3d64fd6f9f0d31fdf5b46a
vuongquocnguyen <nguvuong@sheridancollege.ca>
2023-05-25 11:19:08 -0400

    Second commit

--- src/demogit2/DemoGit2.java
+++ src/demogit2/DemoGit2.java
@@ -15,6 +15,7 @@ public class DemoGit2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("First commit");
     }
     
 }
